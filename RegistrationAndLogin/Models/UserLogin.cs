dc4954dba48ac78c148e4ebb0d9a7961fdac7e37
Tanvir Ahmed <75077632+Tanvirt073@users.noreply.github.com>
2023-03-08 15:15:54 +0600

    first ever commit

---
+++ RegistrationAndLogin/Models/UserLogin.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace RegistrationAndLogin.Models
+{
+    public class UserLogin
+    {
+        [DisplayName("EmailID")]
+        [Required(AllowEmptyStrings = false, ErrorMessage ="Email ID required")]
+        public string EmailID { get; set; }
+
+        [DisplayName("Password")]
+        [DataType(DataType.Password)]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Password is required")]
+        public string Password { get; set; }
+
+        [DisplayName("RememberMe")]
+        public bool RememberMe { get; set; }
+    }
+}
